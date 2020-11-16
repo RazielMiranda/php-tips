0b877d3c1292a2722f136af45b7ad35e1a159adf
Raziel M <raziel@upoutsourcing.com>
2020-11-16 00:01:28 -0300

    customização final da classe de email

--- source/Support/Email.php
+++ source/Support/Email.php
@@ -13,10 +13,62 @@ class Email
 
     public function __construct()
     {
-        $this->mail = new PHPMailer();
+        $this->mail = new PHPMailer(exception: true);
         $this->data = new stdClass();
+
         $this->mail->isSMTP();
-        $this->mail->setLanguage( langcode: "br");
+        $this->mail->isHTML();
+
+        // $this->mail->setLanguage( langcode: 'br');
+        $this->mail->SMTPAuth = true;
+        $this->mail->SMTPSecure = "tls";
+        $this->mail->CharSet = "utf-8";
+
+        $this->mail->Host = MAIL["host"];
+        $this->mail->Port = MAIL["port"];
+        $this->mail->Username = MAIL["user"];
+        $this->mail->Password = MAIL["password"];
     }
 
+    public function add(string $subject, string $body, string $recipient_name, string $recipient_email) : Email
+    {
+        $this->data->subject = $subject;
+        $this->data->body = $body;
+        $this->data->recipient_name = $recipient_name;
+        $this->data->recipient_email = $recipient_email;
+
+        return $this;
+    }
+
+    public function attach(string $filePath, string $fileName): Email
+    {
+        $this->data->attach[$filePath] = $fileName;
+    }
+
+    public function send(string $from_name = MAIL['from_name'], string $from_email = MAIL['from_email']): bool
+    {
+        try {
+            $this->mail->Subject = $this->data->subject;
+            $this->mail->msgHTML($this->data->body);
+            $this->mail->addAdrress($this->data->recipient_email, $this->data->recipient_name);
+            $this->mail->setFrom($from_email, $from_name);
+
+            if(!empty($this->data->attach)){
+                foreach($this->data->attach as $path => $name){
+                    $this->mail->addAttachment($path, $name);
+                }
+            }
+
+            $this->mail->send();
+
+        } catch (Exception $exception) {
+            $this->error = $exception;
+            return false;
+        }
+    }
+
+    public function error(): ?Exception
+    {
+        return $this->error;
+    }
 }
