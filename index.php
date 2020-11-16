14800bcc307903e4f62f2609669967ae43d5b88c
Raziel M <raziel@upoutsourcing.com>
2020-11-16 00:14:41 -0300

    fazendo com anexos

--- index.php
+++ index.php
@@ -5,11 +5,29 @@ use Source\Support\Email;
 
 $email = new Email();
 
+//! DISPARO NORMAL
+// $email->add(
+//     subject: "ola",
+//     body: "<p>oi</p>",
+//     recipient_name: "Raziel",
+//     recipient_email: "razielx3@live.com"
+// )->send();
+
+// if (!$email->error()) {
+//     var_dump(expression: true);
+// }else{
+//     echo $email->error()->getMessage();
+// }
+
+//! DISPARO ANEXO
 $email->add(
     subject: "ola",
     body: "<p>oi</p>",
     recipient_name: "Raziel",
     recipient_email: "razielx3@live.com"
+)->attach(
+    filePath: "01.png",
+    fileName: "foto"
 )->send();
 
 if (!$email->error()) {
